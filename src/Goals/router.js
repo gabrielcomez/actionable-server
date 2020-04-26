b5940562374da02d3aca37d16b76fabf5597f0dc
gabrielcomez <gabri.jci@gmail.com>
2020-04-26 15:19:42 +0200

    goals router modified to fetch goals by user

--- src/Goals/router.js
+++ src/Goals/router.js
@@ -4,9 +4,14 @@ const Goal = require("./model");
 
 const router = new Router();
 
-router.post("/goal", auth, async (request, response, next) => {
+router.post("/goal/:eventId", auth, async (request, response, next) => {
   try {
-    const newGoal = { ...request.body, userId: request.user.dataValues.id };
+    console.log(">>>goal adding response", response);
+
+    const newGoal = {
+      ...request.body,
+      userId: request.user.dataValues.id,
+    };
     const goal = await Goal.create(newGoal);
 
     response.status(201).send(goal);
@@ -15,16 +20,25 @@ router.post("/goal", auth, async (request, response, next) => {
   }
 });
 
-router.get("/goal", async (request, response) => {
-  Goal.findAll()
+router.get("/goal/:eventId", auth, async (request, response) => {
+  console.log(">>>request @goalsRouter GET", request.user.dataValues.id);
+
+  Goal.findAll({ where: { userId: request.user.dataValues.id } })
     .then((goals) => {
       if (!goals) {
         response.status(404).send("Oops, no goals here!").end();
       } else {
-        response.send(goals);
+        return response.send(goals);
       }
     })
     .catch((error) => next(error));
 });
 
 module.exports = router;
+
+// /* DELETE individual article. */
+// router.delete("/goal", auth, async (request, response) => {
+//   Goal.findByPk(request.body.eventId).then((goal) => {
+//     goal.destroy();
+//   });
+// });
