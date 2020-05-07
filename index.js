73e953f04b95ccc340a4c629e8a7c4bcd348043a
gabrielcomez <gabri.jci@gmail.com>
2020-05-07 14:58:25 +0200

    dotenv installed and .env variables added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+require("dotenv").config();
 
 const cors = require("cors");
 const corsMiddleware = cors();
