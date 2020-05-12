f492c68368f14752f87def617a6f97327ef1ee26
gabrielcomez <gabri.jci@gmail.com>
2020-05-12 13:19:57 +0200

    added try and catch blocks to the events router

--- src/Events/router.js
+++ src/Events/router.js
@@ -8,33 +8,43 @@ const apiKey = process.env.APP_KEY;
 require("tls").DEFAULT_MIN_VERSION = "TLSv1"; //API node version
 
 router.get("/category", async (req, res) => {
-  const response = await axios(
-    `https://api.eventful.com/json/categories/list?app_key=${apiKey}`
-  );
-  // console.log("response.data @category router", response.data);
-  res.send(response.data);
+  try {
+    const response = await axios(
+      `https://api.eventful.com/json/categories/list?app_key=${apiKey}`
+    );
+    // console.log("response.data @category router", response.data);
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //get all of the events by location
 router.get("/events/:location", async (req, res) => {
-  // console.log("req.params @event location router", req.params);
-
-  const response = await axios(
-    `https://api.eventful.com/json/events/search\?app_key=${apiKey}\&location=${req.params.location}`
-  );
-  // console.log("response.data @event router", response.data);
-  res.send(response.data.events.event);
+  try {
+    const response = await axios(
+      `https://api.eventful.com/json/events/search\?app_key=${apiKey}\&location=${req.params.location}`
+    );
+    // console.log("response.data @event router", response.data);
+    res.send(response.data.events.event);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //get one event
 router.get("/event/:id", async (req, res) => {
-  // console.log("req.params @event event router", req.params);
+  try {
+    // console.log("req.params @event event router", req.params);
 
-  const response = await axios(
-    `https://api.eventful.com/json/events/get\?app_key=${apiKey}\&id\=${req.params.id}`
-  );
-  // console.log("response.data @event router", response.data);
-  res.send(response.data);
+    const response = await axios(
+      `https://api.eventful.com/json/events/get\?app_key=${apiKey}\&id\=${req.params.id}`
+    );
+    // console.log("response.data @event router", response.data);
+    res.send(response.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
